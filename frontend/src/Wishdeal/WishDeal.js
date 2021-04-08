33e0a5b6ec748ee6b2c62868b6d95dc500c3dc6a
CEOJINSUNG <jinsung1048@gmail.com>
2021-04-08 15:59:01 +0900

    Update WishDeal.js

--- frontend/src/Wishdeal/WishDeal.js
+++ frontend/src/Wishdeal/WishDeal.js
@@ -17,118 +17,127 @@ export default function WishDeal() {
                     backgroundColor: "#f2f3f8"
                 }}>
                     <WebIntro />
+                    {/* 절반을 나눔 */}
                     <div style={{
+                        width: "50%",
+                        minWidth: 480,
                         display: "flex",
                         flexDirection: "column",
-                        
-                        justifyContent: "flex-start",
-
-                        width: 480,
-                        height: "100vh",
-                        backgroundColor: "#ffffff",
+                        alignItems: "flex-start",
                     }}>
-                        <Header content="위시딜" />
                         <div style={{
-                            marginLeft:20,
-                            marginTop: 32,
-                            fontWeight:"bold",
-                            fontSize:18
-                        }}>사고싶은 상품 링크를 입력해주세요!</div>
-                        <div>
-                            <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
-                                <input style={{
-                                    width:440,
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    }}
-                                name="link"
-                                placeholder="링크"
-                                >
-                                </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
-                            </div>
-                            
-                        </div>
-                        <div style={{
-                            width: 440,
-                            height: 212,
-                            marginLeft: 20,
-                            marginRight: 20,
-                            marginTop: 32,
-                            backgroundColor: "#f2f3f8",
-                        }}></div>
-                        <div style={{
-                            marginTop:18,
-                            marginLeft:46,
-                            fontSize:14
-                        }}> 이 상품이 맞는지 한번 더 확인해주세요.</div>
-                        <div style={{
-                            marginTop:36,
-                            marginLeft:20,
-                            fontWeight:"bold",
-                            fontSize:18
-                        }}>상품 카테고리를 알려주세요!</div>
-                        <div style={{
-                            display:"flex",
-                            flexDirection:"row",
-                            alignItems:"center",
-                            marginTop:16
-                        }}>
-                            <div style={{width:93,height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:93,height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:93,height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:93,height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                        </div>
-                        <div>
-                            <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
-                                <input style={{
-                                    width:440,
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    
-                                    }}
-                                name="ext"
-                                placeholder="기타 항목을 입력해주세요."
-                                >
-                                </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
-                            </div>
-                            
-                        </div>
-                        <div style={{
-                            borderRadius: 8,
-                            width: 440,
-                            height: 56,
-                            marginLeft: 20,
-                            marginRight: 20,
-                            marginTop: 32,
-                            backgroundColor: "#2dd9d3",
                             display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#ffffff",
-                            fontSize: 18,
-                            fontWeight: "bold",
-                        }}><div style={{
-                            color: "#ffffff",
-                            fontSize: 18,
-                           fontWeight: "bold",
-                        }}>다음</div></div>
+                            flexDirection: "column",
+
+                            justifyContent: "flex-start",
+
+                            width: 480,
+                            height: "100vh",
+                            backgroundColor: "#ffffff",
+                        }}>
+                            <Header content="위시딜" />
+                            <div style={{
+                                marginLeft: 20,
+                                marginTop: 32,
+                                fontWeight: "bold",
+                                fontSize: 18
+                            }}>사고싶은 상품 링크를 입력해주세요!</div>
+                            <div>
+                                <div
+                                    style={{
+                                        marginTop: 16,
+                                        marginLeft: 20,
+                                        marginRight: 20
+                                    }}>
+                                    <input style={{
+                                        width: 440,
+                                        height: 26,
+                                        border: "0px solid #ffffff"
+                                    }}
+                                        name="link"
+                                        placeholder="링크"
+                                    >
+                                    </input>
+                                    <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
+                                </div>
+
+                            </div>
+                            <div style={{
+                                width: 440,
+                                height: 212,
+                                marginLeft: 20,
+                                marginRight: 20,
+                                marginTop: 32,
+                                backgroundColor: "#f2f3f8",
+                            }}></div>
+                            <div style={{
+                                marginTop: 18,
+                                marginLeft: 46,
+                                fontSize: 14
+                            }}> 이 상품이 맞는지 한번 더 확인해주세요.</div>
+                            <div style={{
+                                marginTop: 36,
+                                marginLeft: 20,
+                                fontWeight: "bold",
+                                fontSize: 18
+                            }}>상품 카테고리를 알려주세요!</div>
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                alignItems: "center",
+                                marginTop: 16
+                            }}>
+                                <div style={{ width: 93, height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                                <div style={{ width: 93, height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                                <div style={{ width: 93, height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                                <div style={{ width: 93, height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            </div>
+                            <div>
+                                <div
+                                    style={{
+                                        marginTop: 16,
+                                        marginLeft: 20,
+                                        marginRight: 20
+                                    }}>
+                                    <input style={{
+                                        width: 440,
+                                        height: 26,
+                                        border: "0px solid #ffffff"
+
+                                    }}
+                                        name="ext"
+                                        placeholder="기타 항목을 입력해주세요."
+                                    >
+                                    </input>
+                                    <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
+                                </div>
+
+                            </div>
+                            <div style={{
+                                borderRadius: 8,
+                                width: 440,
+                                height: 56,
+                                marginLeft: 20,
+                                marginRight: 20,
+                                marginTop: 32,
+                                backgroundColor: "#2dd9d3",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                color: "#ffffff",
+                                fontSize: 18,
+                                fontWeight: "bold",
+                            }}><div style={{
+                                color: "#ffffff",
+                                fontSize: 18,
+                                fontWeight: "bold",
+                            }}>다음</div></div>
+                        </div>
                     </div>
                 </div>
             </Default>
             <Mobile>
-            <div style={{
+                <div style={{
                     display: "flex",
                     flexDirection: "row",
                     alignItems: "center",
@@ -140,7 +149,7 @@ export default function WishDeal() {
                     <div style={{
                         display: "flex",
                         flexDirection: "column",
-                        
+
                         justifyContent: "flex-start",
 
                         width: "100%",
@@ -149,30 +158,30 @@ export default function WishDeal() {
                     }}>
                         <Header content="위시딜" />
                         <div style={{
-                            marginLeft:20,
+                            marginLeft: 20,
                             marginTop: 32,
-                            fontWeight:"bold",
-                            fontSize:18
+                            fontWeight: "bold",
+                            fontSize: 18
                         }}>사고싶은 상품 링크를 입력해주세요!</div>
                         <div>
                             <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
+                                style={{
+                                    marginTop: 16,
+                                    marginLeft: 20,
+                                    marginRight: 20
+                                }}>
                                 <input style={{
-                                    width:"90%",
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    }}
-                                name="link"
-                                placeholder="링크"
+                                    width: "90%",
+                                    height: 26,
+                                    border: "0px solid #ffffff"
+                                }}
+                                    name="link"
+                                    placeholder="링크"
                                 >
                                 </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
+                                <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
                             </div>
-                            
+
                         </div>
                         <div style={{
                             width: "90%",
@@ -183,47 +192,47 @@ export default function WishDeal() {
                             backgroundColor: "#f2f3f8",
                         }}></div>
                         <div style={{
-                            marginTop:18,
-                            marginLeft:46,
-                            fontSize:14
+                            marginTop: 18,
+                            marginLeft: 46,
+                            fontSize: 14
                         }}> 이 상품이 맞는지 한번 더 확인해주세요.</div>
                         <div style={{
-                            marginTop:36,
-                            marginLeft:20,
-                            fontWeight:"bold",
-                            fontSize:18
+                            marginTop: 36,
+                            marginLeft: 20,
+                            fontWeight: "bold",
+                            fontSize: 18
                         }}>상품 카테고리를 알려주세요!</div>
                         <div style={{
-                            display:"flex",
-                            flexDirection:"row",
-                            alignItems:"center",
-                            marginTop:16
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            marginTop: 16
                         }}>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
                         </div>
                         <div>
                             <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
+                                style={{
+                                    marginTop: 16,
+                                    marginLeft: 20,
+                                    marginRight: 20
+                                }}>
                                 <input style={{
-                                    width:"90%",
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    
-                                    }}
-                                name="ext"
-                                placeholder="기타 항목을 입력해주세요."
+                                    width: "90%",
+                                    height: 26,
+                                    border: "0px solid #ffffff"
+
+                                }}
+                                    name="ext"
+                                    placeholder="기타 항목을 입력해주세요."
                                 >
                                 </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
+                                <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
                             </div>
-                            
+
                         </div>
                         <div style={{
                             borderRadius: 8,
@@ -242,7 +251,7 @@ export default function WishDeal() {
                         }}><div style={{
                             color: "#ffffff",
                             fontSize: 18,
-                           fontWeight: "bold",
+                            fontWeight: "bold",
                         }}>다음</div></div>
                     </div>
                 </div>
