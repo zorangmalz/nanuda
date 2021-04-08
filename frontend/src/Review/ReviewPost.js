7738eb6a50e5ef798761c23e3da346c8838d6659
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-04-08 17:59:33 +0900

    Update ReviewPost.js

--- frontend/src/Review/ReviewPost.js
+++ frontend/src/Review/ReviewPost.js
@@ -36,62 +36,70 @@ export default function WishDeal() {
                             backgroundColor: "#ffffff",
                         }}>
                             <Header content="나눠산 사람들" />
-                            <div style={{display:"flex", flexDirection:"row",justifyContent:"space-between"}}>
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                                <div style={{
+                                    display: "flex",
+                                    flexDirection: "row"
+                                }}>
+                                    <div style={{
+                                        width: 32,
+                                        height: 32,
+                                        backgroundColor: "#f2f3f8",
+                                        marginTop: 32,
+                                        marginLeft: 20,
+                                        borderRadius: 16
+                                    }}>
+                                    </div>
+                                    <div style={{
+                                        marginTop: 38,
+                                        marginLeft: 8,
+                                        fontFamily: "AvenirNext",
+                                        fontWeight: "bold",
+                                        fontSize: 14
+                                    }}>나누다홀릭</div>
+                                </div>
+                                <div style={{
+                                    width: 32,
+                                    height: 32,
+                                    backgroundColor: "#f2f3f8",
+                                    marginTop: 32,
+                                    marginRight: 20,
+                                    borderRadius: 16
+                                }}>
+                                </div>
+                            </div>
+
+                            <div style={{ width: 480, height: 500, backgroundColor: "#2dd9d3", marginTop: 8 }}></div>
+
                             <div style={{
-                                display:"flex",
-                                flexDirection:"row"
-                            }}>                          
-                           <div style={{
-                            width:32,
-                            height:32,
-                            backgroundColor:"#f2f3f8",
-                            marginTop:32,
-                            marginLeft:20,
-                            borderRadius:16
-                        }}>
+                                marginTop: 16,
+                                display: "flex",
+                                flexDirection: "row"
+                            }}>
+                                <div style={{
+                                    width: 96,
+                                    height: 96,
+                                    backgroundColor: "#dfdfdf",
+                                    marginLeft: 20,
+                                    borderRadius: 6
+                                }}></div>
+                                <div style={{
+                                    marginLeft: 14,
+                                    display:"flex",
+                                    flexDirection:"column"
+                                }}>
+                                    <div style={{ fontSize: 14, fontFamily: "AvenirNext", marginBottom: 8 }}>삼베옷 컬렉션, White, 95</div>
+                                    <div style={{ fontSize: 14, opacity: 0.6, textDecoration: "line-through", marginBottom: 8 }}>210,000 원</div>
+                                    <div style={{ fontSize: 16, fontWeight: "bold", color: "#051a1a", marginBottom: 8 }}>70,000 원에 획득 완료!</div>
+                                    <div style={{ fontSize: 14, color:"#26c1f0",fontWeight:"bold", }}>10 명이 따라 샀어요!</div>
+                                </div>
                             </div>
-                           <div style={{
-                               marginTop:38,
-                               marginLeft:8,
-                               fontFamily:"AvenirNext",
-                               fontWeight:"bold",
-                               fontSize:14
-                           }}>나누다홀릭</div>
-                           </div>
-                           <div style={{
-                            width:32,
-                            height:32,
-                            backgroundColor:"#f2f3f8",
-                            marginTop:32,
-                            marginRight:20,
-                            borderRadius:16
-                        }}>
-                            </div>
-                           </div>
-
-                           <div style={{width:480,height:500,backgroundColor:"#2dd9d3",marginTop:8}}></div>
-
-                           <div style={{
-                               marginTop:16,
-                               display:"flex",
-                               flexDirection:"row"
-                           }}>
-                               <div style={{
-                                   width:96,
-                                   height:96,
-                                    backgroundColor:"#dfdfdf",
-                                    marginLeft:20,
-                                    borderRadius:6
-                               }}></div>
-                               <div style={{
-                                   marginLeft:14
-                               }}>
-                                   <div style={{fontSize:14,fontFamily:"AvenirNext",marginBottom:12}}>삼베옷 컬렉션, White, 95</div>
-                                   <div style={{fontSize:14,opacity:0.6,textDecoration:"line-through",marginBottom:12}}>210,000 원</div>
-                                   <div styel={{fontSize:16,fontWeight:"bold",color:"#051a1a",marginBottom:12}}>70,000 원에 획득 완료!</div>
-                                   <div style={{fontSize:14}}>10 명이 따라 샀어요!</div>
-                               </div>
-                           </div>
+                            <div style={{
+                                marginTop:16,
+                                width:480,
+                                border:"0.5px solid #051a1a",
+                                opacity:0.2
+                            }}></div>
                             <div style={{
                                 borderRadius: 8,
                                 width: 440,
@@ -116,7 +124,7 @@ export default function WishDeal() {
                 </div>
             </Default>
             <Mobile>
-            <div style={{
+                <div style={{
                     display: "flex",
                     flexDirection: "row",
                     alignItems: "center",
@@ -128,7 +136,7 @@ export default function WishDeal() {
                     <div style={{
                         display: "flex",
                         flexDirection: "column",
-                        
+
                         justifyContent: "flex-start",
 
                         width: "100%",
@@ -137,30 +145,30 @@ export default function WishDeal() {
                     }}>
                         <Header content="위시딜" />
                         <div style={{
-                            marginLeft:20,
+                            marginLeft: 20,
                             marginTop: 32,
-                            fontWeight:"bold",
-                            fontSize:18
+                            fontWeight: "bold",
+                            fontSize: 18
                         }}>사고싶은 상품 링크를 입력해주세요!</div>
                         <div>
                             <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
+                                style={{
+                                    marginTop: 16,
+                                    marginLeft: 20,
+                                    marginRight: 20
+                                }}>
                                 <input style={{
-                                    width:"90%",
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    }}
-                                name="link"
-                                placeholder="링크"
+                                    width: "90%",
+                                    height: 26,
+                                    border: "0px solid #ffffff"
+                                }}
+                                    name="link"
+                                    placeholder="링크"
                                 >
                                 </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
+                                <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
                             </div>
-                            
+
                         </div>
                         <div style={{
                             width: "90%",
@@ -171,47 +179,47 @@ export default function WishDeal() {
                             backgroundColor: "#f2f3f8",
                         }}></div>
                         <div style={{
-                            marginTop:18,
-                            marginLeft:46,
-                            fontSize:14
+                            marginTop: 18,
+                            marginLeft: 46,
+                            fontSize: 14
                         }}> 이 상품이 맞는지 한번 더 확인해주세요.</div>
                         <div style={{
-                            marginTop:36,
-                            marginLeft:20,
-                            fontWeight:"bold",
-                            fontSize:18
+                            marginTop: 36,
+                            marginLeft: 20,
+                            fontWeight: "bold",
+                            fontSize: 18
                         }}>상품 카테고리를 알려주세요!</div>
                         <div style={{
-                            display:"flex",
-                            flexDirection:"row",
-                            alignItems:"center",
-                            marginTop:16
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            marginTop: 16
                         }}>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
-                            <div style={{width:"18%",height:74,marginLeft:20,borderRadius:6,border:"solid 1px #707070"}}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
+                            <div style={{ width: "18%", height: 74, marginLeft: 20, borderRadius: 6, border: "solid 1px #707070" }}></div>
                         </div>
                         <div>
                             <div
-                             style={{
-                                 marginTop:16,
-                                marginLeft:20,
-                                marginRight:20
-                                                 }}>
+                                style={{
+                                    marginTop: 16,
+                                    marginLeft: 20,
+                                    marginRight: 20
+                                }}>
                                 <input style={{
-                                    width:"90%",
-                                    height:26,
-                                    border:"0px solid #ffffff"
-                                    
-                                    }}
-                                name="ext"
-                                placeholder="기타 항목을 입력해주세요."
+                                    width: "90%",
+                                    height: 26,
+                                    border: "0px solid #ffffff"
+
+                                }}
+                                    name="ext"
+                                    placeholder="기타 항목을 입력해주세요."
                                 >
                                 </input>
-                                <div style={{width:438,marginTop:7,height:0,border:"solid 1px #f2f3f8"}}></div>
+                                <div style={{ width: 438, marginTop: 7, height: 0, border: "solid 1px #f2f3f8" }}></div>
                             </div>
-                            
+
                         </div>
                         <div style={{
                             borderRadius: 8,
@@ -230,7 +238,7 @@ export default function WishDeal() {
                         }}><div style={{
                             color: "#ffffff",
                             fontSize: 18,
-                           fontWeight: "bold",
+                            fontWeight: "bold",
                         }}>다음</div></div>
                     </div>
                 </div>
