2527b47b0a769d1907562b482b03b607a6d8bcf9
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-04-21 16:42:21 +0900

    max

--- backend/nanuda/models.py
+++ backend/nanuda/models.py
@@ -23,7 +23,7 @@ class User(models.Model):
     address_detail = models.TextField()
     point = models.IntegerField()
     # 은행에 대한 choice 필요
-    bank = models.CharField()
+    bank = models.CharField(max_length=30)
     account = models.CharField(max_length=30)
 
     class Meta:
