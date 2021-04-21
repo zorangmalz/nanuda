a94f5ae38fb7f4d8fbbb490220dd382ed82955bb
CEOJINSUNG <jinsung1048@gmail.com>
2021-04-21 13:55:00 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_URL = os.path.join(BASE_DIR, "/static/")
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
