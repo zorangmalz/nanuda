ac923c6f88b144d6a0f48f7367ead78c809b3879
CEOJINSUNG <jinsung1048@gmail.com>
2021-04-22 17:23:01 +0900

    push

--- backend/nanuda/views.py
+++ backend/nanuda/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
-
-# API 제작 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from nanuda.models import User, ServiceReview, Product, Review, Order
-from nanuda.serializers import UserAllSerializer, ServicReviewAllSerializer, ProductAllSerializer, ReviewAllSerializer, OrderAllSerializer
-from django.shortcuts import redirect
 from django.views.generic import View
 from django.http import HttpResponse
 import requests
 import json
+
+
+# API 제작 
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from nanuda.models import User, ServiceReview, Product, Review, Order
+from nanuda.serializers import UserAllSerializer, ServicReviewAllSerializer, ProductAllSerializer, ReviewAllSerializer, OrderAllSerializer
 
 # Create your views here.
 # FacebookLogin
@@ -27,6 +29,8 @@ class GoogleLogin(SocialLoginView):
 
 # User Information
 @api_view(["GET", "POST"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def user_list(request):
     if request.method == "GET":
         users = User.objects.all()
@@ -39,6 +43,24 @@ def user_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ServiceReview
+@api_view(["GET", "POST"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def service_review_all(request):
+    if request.method == "GET":
+        service_review = ServiceReview.objects.all()
+        service_review_serializer = ServicReviewAllSerializer(service_review, many=True)
+        return Response(service_review_serializer.data)
+    
+    elif request.method == "POST":
+        service_review_serializer = ServicReviewAllSerializer(data=request.data)
+        if request.user.is_authenticated and service_review_serializer.is_valid():
+            service_review_serializer.save()
+            return Response(service_review_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(service_review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
