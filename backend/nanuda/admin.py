adee2e9450b3fd55590ffecea06e4037514fddad
CEOJINSUNG <jinsung1048@gmail.com>
2021-04-22 10:59:02 +0900

    admin 추가

--- backend/nanuda/admin.py
+++ backend/nanuda/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, ServiceReview, Product, Review, Order
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(ServiceReview)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
