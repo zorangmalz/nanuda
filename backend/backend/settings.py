fcf95708c63f649238e92ee71c3a370c782b394c
CEOJINSUNG <jinsung1048@gmail.com>
2021-05-27 11:26:55 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,4 +162,6 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_URL = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
