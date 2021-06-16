8d61ebcd46bd86d4afe8a15e10fa22cbefa024df
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-06-16 17:54:33 +0900

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     path('checkAddress/', checkAddress.as_view(), name='address'),
     path('servicereviewornot/', serviceReviewOrNOt.as_view(), name='service'),
     path('orderupload/', orderUpload.as_view(), name='orderupload'),
-    path('niceMain/', orderUpload.as_view(), name='niceMain'),
+    path('niceMain/', niceMain.as_view(), name='niceMain'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
