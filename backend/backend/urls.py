ef7d878331dbbc11986f59712e3fa806ecd6f705
CEOJINSUNG <jinsung1048@gmail.com>
2021-06-23 15:11:56 +0900

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,13 +20,14 @@ from nanuda.views import KakaoLogin, userInfoName, uploadAddress, checkAddress, 
 #API 제작
 from rest_framework.urlpatterns import format_suffix_patterns
 from nanuda.views import KakaoLogin
-from data.views import order_one, point_list, review_profile, user_list, service_review_all, product_all, review_all, order_all, review_one, review_home, service_review_home, user_one, order_list
+from data.views import mission_all, order_one, point_list, review_profile, user_list, service_review_all, product_all, review_all, order_all, review_one, review_home, service_review_home, user_one, order_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', user_list),
     path('userinfo/', user_one),
     path('userpoint', point_list),
+    path('user/mission', mission_all),
     path('servicereview/main', service_review_all),
     path('servicereview/home', service_review_home),
     path('product/', product_all),
