99323411d94698e64c3c941fdbdc92eaebf560eb
CEOJINSUNG <jinsung1048@gmail.com>
2021-06-24 16:20:47 +0900

    Update views.py

--- backend/data/views.py
+++ backend/data/views.py
@@ -269,7 +269,7 @@ def review_one(request, pk):
         return Response(review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        order = Order.objects.get(order_id = review.order_id)
+        order = Order.objects.get(id = review.order_id)
         order.review_write = True
         order.save()
         review.delete()
