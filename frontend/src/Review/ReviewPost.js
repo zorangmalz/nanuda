0af76ee8bd37396067b8ac2e308a728844d27dcd
CEOJINSUNG <jinsung1048@gmail.com>
2021-06-28 16:38:12 +0900

    Update ReviewPost.js

--- frontend/src/Review/ReviewPost.js
+++ frontend/src/Review/ReviewPost.js
@@ -38,7 +38,7 @@ export default function ReviewPost({ match }) {
             },
             credentials: "include",
         })
-            .then(response => response.json())
+            .then(response => response.text())
             .then(response => {
                 console.log(response)
                 setData({
