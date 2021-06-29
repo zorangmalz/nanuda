b7b2c72fba2b23e2deb05ec8fc6fefa952b77b78
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-06-29 16:56:42 +0900

    d

--- frontend/src/MyProfile/ProfilePaymentDetail.js
+++ frontend/src/MyProfile/ProfilePaymentDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import { Default, Mobile } from "../App";
 import WebIntro, { Header, MHeader } from "../Style";
 import { useHistory ,useLocation} from "react-router";
