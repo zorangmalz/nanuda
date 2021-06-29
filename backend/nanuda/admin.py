468b6aa56c72b559ebff3edbb862dceb674b5bde
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-06-29 14:19:11 +0900

    Update admin.py

--- backend/nanuda/admin.py
+++ backend/nanuda/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from nanuda.models import Address, MissionList, ReviewList, User, ServiceReview, Product, Review, Order, PointList
+from nanuda.models import Address, MissionList, ReviewList, User, ServiceReview, Product, Review, Order, PointList,WishDeal,PaymentHistory
 
 # Register your models here.
 admin.site.register(User)
@@ -11,3 +11,5 @@ admin.site.register(PointList)
 admin.site.register(Address)
 admin.site.register(MissionList)
 admin.site.register(ReviewList)
+admin.site.register(PaymentHistory)
+admin.site.register(WishDeal)
