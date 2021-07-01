43fa5f10c802fef3e33c876a319f3b7c328963da
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-07-01 12:05:24 +0900

    Update SignupProfile.js

--- frontend/src/Auth/SignupProfile.js
+++ frontend/src/Auth/SignupProfile.js
@@ -47,10 +47,10 @@ export default function SignupProfile() {
     },[])
     function fnPopup(){
         console.log(enc)
-		// window.open('', 'popupChk', 'width=500, height=550, top=100, left=100, fullscreen=no, menubar=no, status=no, toolbar=no, titlebar=yes, location=no, scrollbar=no');
+		window.open('', 'popupChk', 'width=500, height=550, top=100, left=100, fullscreen=no, menubar=no, status=no, toolbar=no, titlebar=yes, location=no, scrollbar=no');
         try{
             document.form_chk.action = "https://nice.checkplus.co.kr/CheckPlusSafeModel/checkplus.cb";
-            // document.form_chk.target = "popupChk";
+            document.form_chk.target = "popupChk";
             document.form_chk.submit();
         }catch(err){
             console.log(err)
